df00c9abb15f18a0e38537075eaf7bb7b4ec1748
Walisson oliveira <walissonoliveira1@outlook.com>
2024-10-17 10:54:35 -0300

    Sessão de quem somos com todas a imagems

--- carrosel.js
+++ carrosel.js
@@ -1,14 +1,59 @@
 const imagens = [
     {
         id: '1',
+        background: 'pet-care-animate.svg',
+        front: 'turma.png'
+    },
+
+    {
+        id: '2',
         background: 'good-team-animate.svg',
         front: 'andre.png'
     },
     {
-        id: '2',
+        id: '3',
         background: 'world-childrens-day-animate.svg',
         front: 'micheline.png'
-    }
+    },
+    {
+       id: '4',
+        background: 'muslim-graduation-animate.svg',
+        front: 'caio (2).png'
+    },
+    {
+        id:'5',
+        background: 'live-collaboration-animate.svg',
+        front:'Vinicius.png'
+
+    },
+    {
+        id:'6',
+        background: 'design-tools-animate.svg',
+        front:'Gaby.png'
+    },
+    {
+        id:'4',
+        background: 'in-progress-animate.svg',
+        front: 'Luiza.png'
+    },
+    {
+        id:'7',
+        background:'in-progress-animate.svg',
+        front:'milene (2).png'
+    },
+    {
+        id: '8',
+        background: 'in-progress-animate.svg',
+        front:'joy.png'
+    },
+    {
+        id:'9',
+        background:'live-collaboration-animate.svg',
+        front:'michel.png'
+    },
+    
+
+
 ];
 
 let currentIndex = 0;
@@ -26,7 +71,7 @@ function criarCarrosel() {
 
         // Atualiza a imagem da frente
         frontImage.src = `img/${imagens[currentIndex].front}`;
-    }, 5000); // 5000 ms = 5 segundos
+    }, 4000); // 4000 ms = 4 segundos
 }
 
 if (imagens[currentIndex].front === 'micheline.png') {
@@ -34,7 +79,8 @@ if (imagens[currentIndex].front === 'micheline.png') {
 } else { 
     frontImage.style.width = '200px'; // Tamanho padrão para outras imagens
 }
+
+
 // Inicia o carrossel quando a página carregar
 window.onload = criarCarrosel;
-
 
